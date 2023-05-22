24d181a4ec1bf218070477e9527210e660018977
William Kwadwo Owusu <87769060+ChelseaAkosuaOwusu@users.noreply.github.com>
2023-05-22 08:29:46 +0000

    sign up and login pages change without reload

--- app.js
+++ app.js
@@ -5,3 +5,29 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+
+   // Form Container 
+   const formArea = document.getElementById('main');
+
+   // Form
+   const loginDiv = document.getElementById('main__content_on_login');
+   const signupDiv = document.getElementById('main__content_on_signup');
+   
+   // Form Switch
+   const OpenLogin = document.getElementById('login__link');
+   const OpenSignup = document.getElementById('signup__link');
+
+   
+   // Show Sign Up Form
+   OpenSignup.addEventListener('click', () => {
+     loginDiv.style.display = 'none';
+     signupDiv.style.display = 'flex';
+   });
+   
+   // Show Login Form
+   OpenLogin.addEventListener('click', () => {
+    loginDiv.style.display = 'flex';
+    signupDiv.style.display = 'none';
+   });
+   
